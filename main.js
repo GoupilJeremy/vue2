68220b7130a81ee641709a50cc04b9a6cb31043f
Jérémy Goupil <goupiljeremy@gmail.com>
2021-01-24 20:49:59 +0100

    Computed Properties

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
-        image: 'assets/vmSocks-green-onWhite.jpg',
-        inStock: true,
+        selectedVariant: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [{
             variantId: 2234,
             variantColor: "green",
-            variantImage: 'assets/vmSocks-green-onWhite.jpg'
+            variantImage: 'assets/vmSocks-green-onWhite.jpg',
+            variantQuantity: 10
         },
         {
             variantId: 2235,
             variantColor: "blue",
-            variantImage: 'assets/vmSocks-blue-onWhite.jpg'
+            variantImage: 'assets/vmSocks-blue-onWhite.jpg',
+            variantQuantity: 0
         }],
         cart: 0
     },
@@ -21,8 +23,19 @@ var app = new Vue({
         addToCart(){
             this.cart += 1
         },
-        updateProduct(variantImage){
-            this.image = variantImage
+        updateProduct(index){
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 });
