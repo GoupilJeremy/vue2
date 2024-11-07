bd3258692246b06d948f77fdbb8c24d11b09ba98
Jérémy Goupil <goupiljeremy@gmail.com>
2024-11-07 10:28:43 +0000

    init: 🎉start

--- README.md
+++
@@ -1,6 +0,0 @@
-# Vue apprentissage
-
-Learn Vue from "Vue Mastery" https://www.vuemastery.com
-
-you can see the result here: https://goupiljeremy.github.io/vue2
-
---
+++ vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const header = ref('Shopping List App')
+</script>
+
+<template>
+ <h1>{{ header }}</h1>
+ <input v-model="msg">
+</template>
+
